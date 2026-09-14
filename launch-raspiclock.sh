ab412b87847e78c89e525087cacd0cb0c7dc9de0
Jim Kelley <131250+KwisatzJim@users.noreply.github.com>
2026-09-14 14:45:09 -0500

    Add Touch Display 2 landscape launcher

---
+++ launch-raspiclock.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/usr/bin/cage -- /home/jim/RaspiClock/target/release/raspiclock &
+CAGE_PID=$!
+
+# Wait briefly for Cage's Wayland output to become available.
+sleep 2
+
+WAYLAND_DISPLAY=wayland-0 \
+XDG_RUNTIME_DIR=/run/user/$(id -u) \
+/usr/bin/wlr-randr --output DSI-1 --transform 90
+
+wait "$CAGE_PID"
